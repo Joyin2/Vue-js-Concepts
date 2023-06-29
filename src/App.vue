056e17e2230a558103d45d4cb9029f7f6224a803
Joyin2 <joyinlaskar2000@gmail.com>
2023-06-29 20:12:26 +0530

    Loop

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <script setup>
-import Event from './components/Event.vue';
-import HelloWorld from './components/HelloWorld.vue'
+// import Event from './components/Event.vue';
+// import HelloWorld from './components/HelloWorld.vue'
 import Home from "./components/Home.vue"
-import IfElse from './components/If-else.vue';
+// import IfElse from './components/If-else.vue';
+import Loop from './components/Loop.vue';
 </script>
 
 <template>
   <div class="container">
     <h1>hello app </h1>
     <Home name="Joyin"/>  <!-- props -->
-    <Event/>
+    <!-- <Event/> -->
 
-    <IfElse/>
+    <!-- <IfElse/> -->
+    <Loop/>
   </div>
   
 </template>
---
+++ src/components/Loop.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        { id: 1, name: 'joyin', email: 'joyin@gmail.com' },
+        { id: 2, name: 'peter', email: 'peter@gmail.com' },
+        { id: 3, name: 'ragnar', email: 'ragnar@gmail.com' },
+        { id: 4, name: 'bjorn', email: 'bjorn@gmail.com' }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>For loop in Vue js</h1>
+    <table>
+      <tr>
+        <td>ID</td>
+        <td>Name</td>
+        <td>Email</td>
+      </tr>
+      <tr v-for="user in users" :key="user.id">
+        <td>{{ user.id }}</td>
+        <td>{{ user.name }}</td>
+        <td>{{ user.email }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
