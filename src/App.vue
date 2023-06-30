2a989be40de36e52a3fec18bef8918ff6117f330
Joyin2 <joyinlaskar2000@gmail.com>
2023-06-30 10:00:28 +0530

    simple data bind

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <script setup>
+import DataBind from './DataBind.vue';
+
 // import Event from './components/Event.vue';
 // import HelloWorld from './components/HelloWorld.vue'
-import Home from "./components/Home.vue"
+// import Home from "./components/Home.vue"
 // import IfElse from './components/If-else.vue';
-import Loop from './components/Loop.vue';
+// import Loop from './components/Loop.vue';
 </script>
 
 <template>
   <div class="container">
     <h1>hello app </h1>
-    <Home name="Joyin"/>  <!-- props -->
+    <!-- props -->
+    <!-- <Home name="Joyin"/>   -->
     <!-- <Event/> -->
 
     <!-- <IfElse/> -->
-    <Loop/>
+    <!-- <Loop/> -->
+    <DataBind/>
+
   </div>
   
 </template>
---
+++ src/DataBind.vue
@@ -0,0 +1,16 @@
+<script>
+export default{
+    data(){
+        return{
+            message:"Hello"
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1 style="white-space: pre-line;">{{ message }}</h1>
+        <textarea type="text" v-model="message" />
+    </div>
+</template>
